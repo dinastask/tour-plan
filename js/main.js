9301b139b82be43aa90c20f9ba4ef5856a297061
dinastask <89831960018pd@mail.ru>
2020-08-10 11:48:18 +0700

    create: aded swiper-sliderin hotel-section

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var mySwiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slaider-button--next",
+    prevEl: ".slaider-button--prev",
+  },
+
+  effect: "cube",
+});
