30070d8a4df0628a6499331cd7621448c145612d
dinastask <89831960018pd@mail.ru>
2020-08-20 20:47:50 +0700

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -59,3 +59,11 @@ function init() {
     )
   );
 }
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log("Клик");
+  document
+    .querySelector(".navbar-bottom")
+    .classList.toggle("navbar-bottom--visible");
+});
