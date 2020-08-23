36679361865edc92056e7e1b7c24fd91fe298ade
dinastask <89831960018pd@mail.ru>
2020-08-23 20:32:03 +0700

    create: added block modal

--- js/main.js
+++ js/main.js
@@ -1,69 +1,88 @@
-var hotelSlaider = new Swiper(".hotel__slider", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
+$(document).ready(function () {
+  var hotelSlaider = new Swiper(".hotel__slider", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".hotel-slider__button--next",
-    prevEl: ".hotel-slider__button--prev",
-  },
-
-  keyboard: {
-    enabled: true,
-    onlyInViewport: true,
-  },
-});
-
-var reviewsSlider = new Swiper(".reviews-slider", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".reviews-slider__button--next",
-    prevEl: ".reviews-slider__button--prev",
-  },
-
-  keyboard: {
-    enabled: true,
-    onlyInViewport: true,
-  },
-});
-
-ymaps.ready(init);
-
-function init() {
-  var myMap = new ymaps.Map(
-    "map",
-    {
-      center: [7.838196, 98.298813],
-      zoom: 15,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
     },
-    {
-      searchControlProvider: "yandex#search",
-    }
-  );
 
-  myMap.geoObjects.add(
-    new ymaps.Placemark(
-      [7.838196, 98.298813],
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+  });
+
+  var reviewsSlider = new Swiper(".reviews-slider", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+  });
+
+  ymaps.ready(init);
+
+  function init() {
+    var myMap = new ymaps.Map(
+      "map",
       {
-        balloonContent: "цвет <strong>воды пляжа бонди</strong>",
+        center: [7.838196, 98.298813],
+        zoom: 15,
       },
       {
-        preset: "islands#icon",
-        iconColor: "#0095b6",
+        searchControlProvider: "yandex#search",
       }
-    )
-  );
-}
+    );
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function () {
-  console.log("Клик");
-  document
-    .querySelector(".navbar-bottom")
-    .classList.toggle("navbar-bottom--visible");
+    myMap.geoObjects.add(
+      new ymaps.Placemark(
+        [7.838196, 98.298813],
+        {
+          balloonContent: "цвет <strong>воды пляжа бонди</strong>",
+        },
+        {
+          preset: "islands#icon",
+          iconColor: "#0095b6",
+        }
+      )
+    );
+  }
+
+  var menuButton = $(".menu-button");
+  menuButton.on("click", function () {
+    $(".navbar-bottom").toggleClass("navbar-bottom--visible");
+  });
+
+  var modalButton = $("[data-toggle=modal]");
+  var closemodalButton = $(".modal__close");
+  console.log(modalButton);
+  modalButton.on("click", openModal);
+  closemodalButton.on("click", closeModal);
+
+  function openModal() {
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
 });
